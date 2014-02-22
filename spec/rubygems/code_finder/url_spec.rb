3be48d9beaf898d9a030f673bc6ceaf25ac17416
sanemat <o.gata.ken@gmail.com>
2014-02-22 23:20:17 +0900

    Repository not found

--- spec/rubygems/code_finder/url_spec.rb
+++ spec/rubygems/code_finder/url_spec.rb
@@ -21,4 +21,11 @@ describe Rubygems::CodeFinder do
 
     expect(Rubygems::CodeFinder.url('mm-devise')).to eq 'http://github.com/kristianmandrup/mm-devise'
   end
+
+  it 'fails repository not found' do
+    stub_request(:get, 'https://rubygems.org/api/v1/gems/fluentd.json').
+      to_return(status: 200, body: File.new('./spec/fixtures/fluentd.json'), headers: {})
+
+    expect { Rubygems::CodeFinder.url('fluentd') }.to raise_error(Rubygems::CodeFinder::RepositoryNotFound)
+  end
 end
