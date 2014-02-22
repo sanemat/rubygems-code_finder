833990b50f55822efd05e20f00e22faaf6f8a456
sanemat <o.gata.ken@gmail.com>
2014-02-22 18:37:27 +0900

    Use uri

--- lib/rubygems-code_finder.rb
+++ lib/rubygems-code_finder.rb
@@ -1,6 +1,7 @@
 require 'rubygems/code_finder/version'
 require 'faraday'
 require 'multi_json'
+require 'uri'
 
 module Rubygems
   module CodeFinder
@@ -11,8 +12,12 @@ module Rubygems
       conn = Faraday.new 'https://rubygems.org'
       response = conn.get('/api/v1/gems/' + name + '.json')
       fail RubygemsNotFound unless response.status == 200
-      body = MultiJson.load(response.body)
-      body['source_code_uri']
+      parse_response_body(response.body)
+    end
+
+    def parse_response_body(body)
+      data = MultiJson.load(body)
+      data['source_code_uri']
     end
   end
 end
